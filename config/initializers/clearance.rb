fa4e82b7dbfc19dbbe4c96a55a6c061992f4f73f
Daniel Kamiński <maniel.rulez@gmail.com>
2013-10-01 10:27:18 +0200

    poprawki w css

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,8 @@
 Clearance.configure do |config|
 	config.cookie_expiration = lambda { 1.year.from_now.utc }
-	config.redirect_url = '/baza/trainings'
+	if Rails.env.production?
+		config.redirect_url = '/baza/trainings'
+	else
+		config.redirect_url = '/trainings'
+	end
 end
