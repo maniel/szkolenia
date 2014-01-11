70760d31d7729f22d06c71050641d2eced9a360d
Daniel Kamiński <maniel.rulez@gmail.com>
2014-01-11 16:27:13 +0100

    role

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 class Organizer < ActiveRecord::Base
+	resourcify
 	has_many :trainings, :dependent => :restrict_with_exception
 	validates_presence_of :name, message: "Organizator nie ma nazwy!"
 end
