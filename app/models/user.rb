35e56ced343e994aa417d6c90ac71626e9bef957
Daniel Kamiński <maniel.rulez@gmail.com>
2014-01-12 21:31:07 +0100

    role - implementacja

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,37 @@
 class User < ActiveRecord::Base
-  rolify
-  resourcify
   include Clearance::User
+  resourcify
+  rolify
 
+  def admin
+  	has_role? :admin
+  end
+
+  alias_method :admin?, :admin
+
+  def admin=(a)
+  	if a
+  		add_role :admin
+  	else
+  		remove_role :admin
+  	end
+  end
+
+  def doradca
+  	has_role? :doradca, User
+  end
+
+  def doradca=(a)
+  	if a
+  		add_role :doradca, User
+  	else
+  		remove_role :doradca, User
+  	end
+  end
+
+  alias_method :admin?, :admin
+
+  alias_method :doradca?, :doradca
 
   def email_optional?
   	true
